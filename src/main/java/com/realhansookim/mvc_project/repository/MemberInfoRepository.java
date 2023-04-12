4024996b457e7991919bf441815932d7cd6c849f
realhansookim <kimhansu115@gmail.com>
2023-04-12 15:37:04 +0900

    AESAlgorythm

--- src/main/java/com/realhansookim/mvc_project/repository/MemberInfoRepository.java
+++ src/main/java/com/realhansookim/mvc_project/repository/MemberInfoRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.realhansookim.mvc_project.entity.MemberInfoEntity;
 
 public interface MemberInfoRepository extends JpaRepository<MemberInfoEntity,Long>{
-    
+    public Integer countByMiId (String MiId);
 }
--- src/main/java/com/realhansookim/mvc_project/service/MemberInfoService.java
+++ src/main/java/com/realhansookim/mvc_project/service/MemberInfoService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.realhansookim.mvc_project.entity.MemberInfoEntity;
 import com.realhansookim.mvc_project.repository.MemberInfoRepository;
+import com.realhansookim.mvc_project.utils.AESAlgorithm;
 import com.realhansookim.mvc_project.vo.LoginAdd;
 import com.realhansookim.mvc_project.vo.LoginVO;
 
@@ -17,6 +18,20 @@ import lombok.RequiredArgsConstructor;
 public class MemberInfoService {
     private final MemberInfoRepository memberInfoRepository;
     public LoginVO addLogin(LoginAdd data){
+        if(memberInfoRepository.countByMiId(data.getId())==1){
+            LoginVO response = LoginVO.builder()
+            .code(HttpStatus.BAD_REQUEST)
+            .status(false)
+            .message(data.getId()+"은/는 이미 등록된 아이디 입니다.")
+            .build();
+            return response;
+        }
+        try {
+            String encPwd = AESAlgorithm.Encrypt(data.getPwd());
+            data.setPwd(encPwd);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         MemberInfoEntity entity = MemberInfoEntity.builder()
         .miId(data.getId()).miPwd(data.getPwd()).miName(data.getName())
         .miBirth(data.getBirth()).build();
